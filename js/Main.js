3fb921f8f59939731fba72ea98973212d7f011a6
gchen <genhan.chen@azgs.az.gov>
2012-06-04 17:21:39 -0700

    Change the basemap, icon css

--- js/Main.js
+++ js/Main.js
@@ -2,9 +2,20 @@ var map;
 function init(){
 	map = new L.Map("map");
 	
-	/* ESRI tiled service */
-	var imgLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer");
-	var boundLayer = new L.TileLayer.ESRI("http://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer");
+	// Cloudmade / OpenStreetMap tiled layer
+	var cmUrl = 'http://{s}.tile.cloudmade.com/f7d28795be6846849741b30c3e4db9a9/997/256/{z}/{x}/{y}.png',
+		cmAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery � <a href="http://cloudmade.com">CloudMade</a>',
+		cmOptions = { maxZoom: 18, attribution: cmAttribution };
+	var cloudmade = new L.TileLayer(cmUrl, cmOptions);
+
+	/* WMS layer */
+	var wmsUrl = "http://opengis.azexperience.org/geoserver/wms";
+		wmsLayer = new L.TileLayer.WMS(wmsUrl, {
+			maxZoom: 10, 
+			layers: "vae:usa", 
+			format: "image/png", 
+			transparent: true 
+		}); 
 	
 	/* WFS GeoJSON layer */
 	var wfsLayer = map.wfsLayer = new L.GeoJSON.WFS("http://opengis.azexperience.org/geoserver/wfs", "vae:ACERT", {
@@ -12,7 +23,7 @@ function init(){
 			return new L.Marker(latlng, { 
 				icon: new L.Icon({ 
 					iconUrl: "style/images/logos/?Agency?png", /// ? + property name used as the image name + ? + image type
-					iconSize: new L.Point(40, 40) 
+					iconSize: new L.Point(50, 50) 
 				}) 
 			});
 		},
@@ -23,7 +34,7 @@ function init(){
 	
 	var center = new L.LatLng(34.1618, -111.53332);
 	
-	map.setView(center, 7).addLayer(imgLayer);
+	map.setView(center, 7).addLayer(cloudmade).addLayer(wmsLayer);
 
 	setTimeout(function(){
 		map.addLayer(boundLayer);},
