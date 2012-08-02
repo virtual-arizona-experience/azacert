4cd09d2c8675ec94c7a94c4c4e547c4c72abbd15
gchen <genhan.chen@azgs.az.gov>
2012-08-02 16:38:54 -0700

    Add other google+1 and twitter plugins

--- js/Main.js
+++ js/Main.js
@@ -16,8 +16,7 @@ function init(){
 	// Basemap
 	var basemapUrl = "http://opengis.azexperience.org/tiles/v2/ACERT/{z}/{x}/{y}.png";
 		basemapAttribution = 'Map data &copy; <a href="http://www.azgs.az.gov/">AZGS</a>' 
-			+ '&nbsp;Contributors: <a href="http://www.azgs.az.gov/">AZGS</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>' 
-			+ '&nbsp;<div style="width: 125px;" class="fb-like" data-send="true" data-layout="button_count" data-width="30" data-show-faces="false" data-font="arial"></div>',
+			+ '&nbsp;Contributors: <a href="http://www.azgs.az.gov/">AZGS</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 		basemapOptions = { maxZoom: 18, attribution: basemapAttribution };
 	var basemap = new L.TileLayer(basemapUrl, basemapOptions);
 
