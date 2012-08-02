d8a723a8a7e330382cdd1180f26608a9f491215f
gchen <genhan.chen@azgs.az.gov>
2012-08-02 13:46:22 -0700

    Do not remove hover control when it does not exist

--- js/azgs-leaflet/GeoJSON.WFS.js
+++ js/azgs-leaflet/GeoJSON.WFS.js
@@ -82,8 +82,9 @@ L.GeoJSON.WFS = L.GeoJSON.extend({
 						this._icon.style.height = this.options.icon.options.iconSize.x + "px";						
 					}
 					//********************************************************************************/	
-					
-					e.layer._hoverControl._map.removeControl(e.layer._hoverControl);
+					if (e.layer._hoverControl){
+						e.layer._hoverControl._map.removeControl(e.layer._hoverControl);
+					}					
 				});
 			}
 			if (e.layer instanceof L.Marker.AttributeFilter) { e.layer.setIcon(e); }
